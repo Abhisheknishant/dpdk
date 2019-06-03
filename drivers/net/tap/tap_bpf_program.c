Abhisheknishant/dpdk

commit 6d13ea8e8e49ab957deae2bba5ecf4a4bfe747d1

net: add rte prefix to ether structures
Add 'rte_' prefix to structures:
- rename struct ether_addr as struct rte_ether_addr.
- rename struct ether_hdr as struct rte_ether_hdr.
- rename struct vlan_hdr as struct rte_vlan_hdr.
- rename struct vxlan_hdr as struct rte_vxlan_hdr.
- rename struct vxlan_gpe_hdr as struct rte_vxlan_gpe_hdr.

Do not update the command line library to avoid adding a dependency to
librte_net.

Signed-off-by: Olivier Matz <olivier.matz@6wind.com>
Reviewed-by: Stephen Hemminger <stephen@networkplumber.org>
Reviewed-by: Maxime Coquelin <maxime.coquelin@redhat.com>
Reviewed-by: Ferruh Yigit <ferruh.yigit@intel.com>

drivers/net/tap/tap_bpf_program.c
@@ -37,7 +37,7 @@
 #define KEY_IDX			0
 #define BPF_MAP_ID_KEY	1
 
-struct vlan_hdr {
+struct rte_vlan_hdr {
 	__be16 proto;
 	__be16 tci;
 };
@@ -141,12 +141,12 @@ rss_l3_l4(struct __sk_buff *skb)
 
 	/* Get correct proto for 802.1ad */
 	if (skb->vlan_present && skb->vlan_proto == htons(ETH_P_8021AD)) {
-		if (data + ETH_ALEN * 2 + sizeof(struct vlan_hdr) +
+		if (data + ETH_ALEN * 2 + sizeof(struct rte_vlan_hdr) +
 		    sizeof(proto) > data_end)
 			return TC_ACT_OK;
 		proto = *(__u16 *)(data + ETH_ALEN * 2 +
-				   sizeof(struct vlan_hdr));
-		off += sizeof(struct vlan_hdr);
+				   sizeof(struct rte_vlan_hdr));
+		off += sizeof(struct rte_vlan_hdr);
 	}
 
 	if (proto == htons(ETH_P_IP)) {

commit d611e0f3dbe5bfb064198b2444b897d0fed485fa

net/tap: revert rte prefixing
This source is compiled as a bpf program out of dpdk.
Revert to structures/macros coming from libc.

Fixes: 6d13ea8e8e49 ("net: add rte prefix to ether structures")
Fixes: 24ac604ef746 ("net: add rte prefix to IP defines")

Signed-off-by: David Marchand <david.marchand@redhat.com>
Acked-by Keith Wiles <keith.wiles@intel.com>
Reviewed-by: Olivier Matz <olivier.matz@6wind.com>

drivers/net/tap/tap_bpf_program.c
@@ -19,7 +19,7 @@
 #include "tap_rss.h"
 
 /** Create IPv4 address */
-#define RTE_IPv4(a, b, c, d) ((__u32)(((a) & 0xff) << 24) | \
+#define IPv4(a, b, c, d) ((__u32)(((a) & 0xff) << 24) | \
 		(((b) & 0xff) << 16) | \
 		(((c) & 0xff) << 8)  | \
 		((d) & 0xff))
@@ -37,7 +37,7 @@
 #define KEY_IDX			0
 #define BPF_MAP_ID_KEY	1
 
-struct rte_vlan_hdr {
+struct vlan_hdr {
 	__be16 proto;
 	__be16 tci;
 };
@@ -141,12 +141,12 @@ rss_l3_l4(struct __sk_buff *skb)
 
 	/* Get correct proto for 802.1ad */
 	if (skb->vlan_present && skb->vlan_proto == htons(ETH_P_8021AD)) {
-		if (data + ETH_ALEN * 2 + sizeof(struct rte_vlan_hdr) +
+		if (data + ETH_ALEN * 2 + sizeof(struct vlan_hdr) +
 		    sizeof(proto) > data_end)
 			return TC_ACT_OK;
 		proto = *(__u16 *)(data + ETH_ALEN * 2 +
-				   sizeof(struct rte_vlan_hdr));
-		off += sizeof(struct rte_vlan_hdr);
+				   sizeof(struct vlan_hdr));
+		off += sizeof(struct vlan_hdr);
 	}
 
 	if (proto == htons(ETH_P_IP)) {
@@ -157,11 +157,11 @@ rss_l3_l4(struct __sk_buff *skb)
 		__u8 *src_dst_addr = data + off + offsetof(struct iphdr, saddr);
 		__u8 *src_dst_port = data + off + sizeof(struct iphdr);
 		struct ipv4_l3_l4_tuple v4_tuple = {
-			.src_addr = RTE_IPv4(*(src_dst_addr + 0),
+			.src_addr = IPv4(*(src_dst_addr + 0),
 					*(src_dst_addr + 1),
 					*(src_dst_addr + 2),
 					*(src_dst_addr + 3)),
-			.dst_addr = RTE_IPv4(*(src_dst_addr + 4),
+			.dst_addr = IPv4(*(src_dst_addr + 4),
 					*(src_dst_addr + 5),
 					*(src_dst_addr + 6),
 					*(src_dst_addr + 7)),