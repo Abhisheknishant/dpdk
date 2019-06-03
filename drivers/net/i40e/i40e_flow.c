Abhisheknishant/dpdk

commit 2e67a7fbf3ffd64cf18a565535135794b62af74d

net/i40e: config flow director automatically
I40e driver needed users to config exact fdir mode to create rte_flow
rules but it shouldn't. This patch allows users to create rte_flow rules
without configuring fdir mode and let the driver config fdir automatically.
And remove the workaround in flow filtering example.

Signed-off-by: Xiaoyun Li <xiaoyun.li@intel.com>
Acked-by: Beilei Xing <beilei.xing@intel.com>

drivers/net/i40e/i40e_flow.c
@@ -3127,6 +3127,7 @@ i40e_flow_parse_fdir_filter(struct rte_eth_dev *dev,
 			    struct rte_flow_error *error,
 			    union i40e_filter_t *filter)
 {
+	struct i40e_pf *pf = I40E_DEV_PRIVATE_TO_PF(dev->data->dev_private);
 	struct i40e_fdir_filter_conf *fdir_filter =
 		&filter->fdir_filter;
 	int ret;
@@ -3148,14 +3149,29 @@ i40e_flow_parse_fdir_filter(struct rte_eth_dev *dev,
 
 	if (dev->data->dev_conf.fdir_conf.mode !=
 	    RTE_FDIR_MODE_PERFECT) {
-		rte_flow_error_set(error, ENOTSUP,
-				   RTE_FLOW_ERROR_TYPE_UNSPECIFIED,
-				   NULL,
-				   "Check the mode in fdir_conf.");
-		return -rte_errno;
+		/* Enable fdir when fdir flow is added at first time. */
+		ret = i40e_fdir_setup(pf);
+		if (ret != I40E_SUCCESS) {
+			rte_flow_error_set(error, ENOTSUP,
+					   RTE_FLOW_ERROR_TYPE_HANDLE,
+					   NULL, "Failed to setup fdir.");
+			return -rte_errno;
+		}
+		ret = i40e_fdir_configure(dev);
+		if (ret < 0) {
+			rte_flow_error_set(error, ENOTSUP,
+					   RTE_FLOW_ERROR_TYPE_HANDLE,
+					   NULL, "Failed to configure fdir.");
+			goto err;
+		}
+
+		dev->data->dev_conf.fdir_conf.mode = RTE_FDIR_MODE_PERFECT;
 	}
 
 	return 0;
+err:
+	i40e_fdir_teardown(pf);
+	return -rte_errno;
 }
 
 /* Parse to get the action info of a tunnel filter
@@ -4708,6 +4724,13 @@ i40e_flow_destroy(struct rte_eth_dev *dev,
 	case RTE_ETH_FILTER_FDIR:
 		ret = i40e_flow_add_del_fdir_filter(dev,
 		       &((struct i40e_fdir_filter *)flow->rule)->fdir, 0);
+
+		/* If the last flow is destroyed, disable fdir. */
+		if (!ret && !TAILQ_EMPTY(&pf->fdir.fdir_list)) {
+			i40e_fdir_teardown(pf);
+			dev->data->dev_conf.fdir_conf.mode =
+				   RTE_FDIR_MODE_NONE;
+		}
 		break;
 	case RTE_ETH_FILTER_HASH:
 		ret = i40e_config_rss_filter_del(dev,
@@ -4899,6 +4922,8 @@ i40e_flow_flush_fdir_filter(struct i40e_pf *pf)
 		     pctype <= I40E_FILTER_PCTYPE_L2_PAYLOAD; pctype++)
 			pf->fdir.inset_flag[pctype] = 0;
 	}
+
+	i40e_fdir_teardown(pf);
 
 	return ret;
 }

drivers/net/i40e/i40e_ethdev.c
@@ -1787,6 +1787,10 @@ i40e_dev_configure(struct rte_eth_dev *dev)
 	ad->tx_simple_allowed = true;
 	ad->tx_vec_allowed = true;
 
+	/* Only legacy filter API needs the following fdir config. So when the
+	 * legacy filter API is deprecated, the following codes should also be
+	 * removed.
+	 */
 	if (dev->data->dev_conf.fdir_conf.mode == RTE_FDIR_MODE_PERFECT) {
 		ret = i40e_fdir_setup(pf);
 		if (ret != I40E_SUCCESS) {
@@ -1844,7 +1848,11 @@ err_dcb:
 	rte_free(pf->vmdq);
 	pf->vmdq = NULL;
 err:
-	/* need to release fdir resource if exists */
+	/* Need to release fdir resource if exists.
+	 * Only legacy filter API needs the following fdir config. So when the
+	 * legacy filter API is deprecated, the following code should also be
+	 * removed.
+	 */
 	i40e_fdir_teardown(pf);
 	return ret;
 }
@@ -2482,6 +2490,11 @@ i40e_dev_close(struct rte_eth_dev *dev)
 	i40e_pf_disable_irq0(hw);
 	rte_intr_disable(intr_handle);
 
+	/*
+	 * Only legacy filter API needs the following fdir config. So when the
+	 * legacy filter API is deprecated, the following code should also be
+	 * removed.
+	 */
 	i40e_fdir_teardown(pf);
 
 	/* shutdown and destroy the HMC */

examples/flow_filtering/main.c
@@ -131,22 +131,6 @@ init_port(void)
 				DEV_TX_OFFLOAD_SCTP_CKSUM  |
 				DEV_TX_OFFLOAD_TCP_TSO,
 		},
-		/*
-		 * Initialize fdir_conf of rte_eth_conf.
-		 * Fdir is used in flow filtering for I40e,
-		 * so rte_flow rules involve some fdir
-		 * configurations. In long term it's better
-		 * that drivers don't require any fdir
-		 * configuration for rte_flow, but we need to
-		 * get this workaround so that sample app can
-		 * run on I40e.
-		 */
-		.fdir_conf = {
-			.mode = RTE_FDIR_MODE_PERFECT,
-			.pballoc = RTE_FDIR_PBALLOC_64K,
-			.status = RTE_FDIR_REPORT_STATUS,
-			.drop_queue = 127,
-		},
 	};
 	struct rte_eth_txconf txq_conf;
 	struct rte_eth_rxconf rxq_conf;

commit ecc928728602dc586aab2e8300c2d218f63c1f73

net/i40e: Fail rte_flow MARK requests if RX func was vectorized
Runtime requests to install an rte_flow with MARK action should fail
if the device was started and a vector RX function was already chosen
for during configuration time.

Currently, i40e rte_flow driver would successfully install the flow
with MARK action, even when vector RX functions are in use. However,
those vector RX functions will fail to retrieve the MARK data from the
device descriptor into the mbuf. The original app installing the flow
would never know what went wrong. The function introduced in this patch
must be adjusted if/when certain vector RX functions start supporting
correct FDIR processing for MARK actions.

Fixes: 2e67a7fbf3ff ("net/i40e: config flow director automatically")
Cc: stable@dpdk.org

Signed-off-by: Mesut Ali Ergin <mesut.a.ergin@intel.com>

v3:
 - Check for support made in an function for future extensibility
 - Fixes code style warnings
 - CC stable w/ fix reference
v2:
 - Check added to fail MARK request only if the device was started
Signed-off-by: 0-day Robot <robot@bytheb.org>

drivers/net/i40e/i40e_flow.c
@@ -22,6 +22,7 @@
 #include "base/i40e_type.h"
 #include "base/i40e_prototype.h"
 #include "i40e_ethdev.h"
+#include "i40e_rxtx.h"
 
 #define I40E_IPV6_TC_MASK	(0xFF << I40E_FDIR_IPv6_TC_OFFSET)
 #define I40E_IPV6_FRAG_HEADER	44
@@ -3040,6 +3041,21 @@ i40e_flow_parse_fdir_pattern(struct rte_eth_dev *dev,
 	return 0;
 }
 
+/* Check if device supports installing a new MARK action */
+static bool i40e_device_supports_mark(struct rte_eth_dev *dev)
+{
+	if (dev->data->dev_started) {
+		/* Vector RX currently doesn't support MARK action. */
+		if (dev->rx_pkt_burst == i40e_recv_scattered_pkts_vec ||
+		    dev->rx_pkt_burst == i40e_recv_pkts_vec ||
+		    dev->rx_pkt_burst == i40e_recv_scattered_pkts_vec_avx2 ||
+		    dev->rx_pkt_burst == i40e_recv_pkts_vec_avx2)
+			return false;
+	}
+
+	return true;
+}
+
 /* Parse to get the action info of a FDIR filter.
  * FDIR action supports QUEUE or (QUEUE + MARK).
  */
@@ -3079,6 +3095,13 @@ i40e_flow_parse_fdir_action(struct rte_eth_dev *dev,
 		filter->action.behavior = I40E_FDIR_PASSTHRU;
 		break;
 	case RTE_FLOW_ACTION_TYPE_MARK:
+		if (!i40e_device_supports_mark(dev)) {
+			/* MARK not supported */
+			rte_flow_error_set(error, EINVAL,
+					   RTE_FLOW_ERROR_TYPE_ACTION, act,
+					   "FDIR Action Error: MARK not supported.");
+			return -rte_errno;
+		}
 		filter->action.behavior = I40E_FDIR_PASSTHRU;
 		mark_spec = act->conf;
 		filter->action.report_status = I40E_FDIR_REPORT_ID;
@@ -3101,6 +3124,13 @@ i40e_flow_parse_fdir_action(struct rte_eth_dev *dev,
 			rte_flow_error_set(error, EINVAL,
 			   RTE_FLOW_ERROR_TYPE_ACTION, act,
 			   "Invalid action.");
+			return -rte_errno;
+		}
+		if (!i40e_device_supports_mark(dev)) {
+			/* MARK not supported */
+			rte_flow_error_set(error, EINVAL,
+					   RTE_FLOW_ERROR_TYPE_ACTION, act,
+					   "FDIR Action Error: MARK not supported.");
 			return -rte_errno;
 		}
 		mark_spec = act->conf;