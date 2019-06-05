c162261d1d078db3e10f263aa4eaa17fcf0fa2e5
Maxime Coquelin <maxime.coquelin@redhat.com>
2019-06-05 12:00:38 +0200

    net/virtio: fix segment data len in mergeable packed Rx path
    Head segment data_len field is wrongly summed with the length
    of all the segments of the chain, whereas it should be the
    length of of the first segment only.
    
    Fixes: a76290c8f1cf ("net/virtio: implement Rx path for packed queues")
    Cc: stable@dpdk.org
    
    Reported-by: Yaroslav Brustinov <ybrustin@cisco.com>
    Reviewed-by: David Marchand <david.marchand@redhat.com>
    Signed-off-by: Maxime Coquelin <maxime.coquelin@redhat.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- drivers/net/virtio/virtio_rxtx.c
+++ drivers/net/virtio/virtio_rxtx.c
@@ -1875,7 +1875,6 @@ virtio_recv_mergeable_pkts_packed(void *rx_queue,
 			rxm->data_len = (uint16_t)(len[i]);
 
 			rx_pkts[nb_rx]->pkt_len += (uint32_t)(len[i]);
-			rx_pkts[nb_rx]->data_len += (uint16_t)(len[i]);
 
 			if (prev)
 				prev->next = rxm;
@@ -1912,7 +1911,6 @@ virtio_recv_mergeable_pkts_packed(void *rx_queue,
 				prev->next = rxm;
 				prev = rxm;
 				rx_pkts[nb_rx]->pkt_len += len[extra_idx];
-				rx_pkts[nb_rx]->data_len += len[extra_idx];
 				extra_idx += 1;
 			}
 			seg_res -= rcv_cnt;
