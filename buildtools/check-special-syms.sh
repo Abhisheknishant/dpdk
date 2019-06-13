aa4446baaddf61da6a1a539f29a52ee6b8b19c5f
Neil Horman <nhorman@tuxdriver.com>
2019-06-13 10:23:35 -0400

    Add __rte_internal tag for functions and version target
    This tag is meant to be used on function prototypes to identify
    functions that are only meant to be used by internal DPDK libraries
    (i.e. libraries that are built while building the SDK itself, as
    identified by the defining of the BUILDING_RTE_SDK macro).  When that
    flag is not set, it will resolve to an error function attribute, causing
    build breakage for any compilation unit attempting to build it
    
    Validate the use of this tag in much the same way we validate
    __rte_experimental.  By adding an INTERNAL version to library map files,
    we can exempt internal-only functions from ABI checking, and handle them
    to ensure that symbols we wish to only be for internal use between dpdk
    libraries are properly tagged with __rte_experimental
    
    Note this patch updates the check-experimental-syms.sh script, which
    normally only check the EXPERIMENTAL section to also check the INTERNAL
    section now.  As such its been renamed to the now more appropriate
    check-special-syms.sh
    
    Signed-off-by: Neil Horman <nhorman@tuxdriver.com>
    CC: Jerin Jacob Kollanukkaran <jerinj@marvell.com>
    CC: Bruce Richardson <bruce.richardson@intel.com>
    CC: Thomas Monjalon <thomas@monjalon.net>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- buildtools/check-special-syms.sh
+++ buildtools/check-special-syms.sh
@@ -31,10 +31,32 @@ do
 		cat >&2 <<- END_OF_MESSAGE
 		$SYM is not flagged as experimental
 		but is listed in version map
-		Please add __rte_experimental to the definition of $SYM
+		Please add __rte_experimental to the definition/prototype of $SYM
 		END_OF_MESSAGE
 		exit 1
 	fi
 done
+
+for i in `awk 'BEGIN {found=0}
+		/.*INTERNAL.*/ {found=1}
+		/.*}.*;/ {found=0}
+		/.*;/ {if (found == 1) print $1}' $MAPFILE`
+do
+	SYM=`echo $i | sed -e"s/;//"`
+	objdump -t $OBJFILE | grep -q "\.text.*$SYM$"
+	IN_TEXT=$?
+	objdump -t $OBJFILE | grep -q "\.text\.internal.*$SYM$"
+	IN_EXP=$?
+	if [ $IN_TEXT -eq 0 -a $IN_EXP -ne 0 ]
+	then
+		cat >&2 <<- END_OF_MESSAGE
+		$SYM is not flagged as internal 
+		but is listed in version map
+		Please add __rte_internal to the definition/prototype of $SYM
+		END_OF_MESSAGE
+		exit 1
+	fi
+done
+
 exit 0
 
