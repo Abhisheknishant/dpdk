46395b18921c3cd83f040b5b8c8bfad6867e3dfb
Stephen Hemminger <stephen@networkplumber.org>
2019-07-01 11:25:32 -0700

    examples/vm_power_manager: fix spelling errors
    Spelling errors found by codespell.
    
    Signed-off-by: Stephen Hemminger <stephen@networkplumber.org>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- examples/vm_power_manager/channel_monitor.c
+++ examples/vm_power_manager/channel_monitor.c
@@ -368,7 +368,7 @@ get_pcpu_to_control(struct policy *pol)
 
 	/*
 	 * So now that we're handling virtual and physical cores, we need to
-	 * differenciate between them when adding them to the branch monitor.
+	 * differentiate between them when adding them to the branch monitor.
 	 * Virtual cores need to be converted to physical cores.
 	 */
 	if (pol->pkt.core_type == CORE_TYPE_VIRTUAL) {
