Abhisheknishant/dpdk

commit 596d31092d322b7a8387f352311ffeb658547914

net: add function to convert string to ethernet address
Make a function that can be used in place of eth_aton_r
to convert a string to rte_ether_addr. This function
allows both byte (xx:xx:xx:xx:xx:xx) and word (XXXX:XXXX:XXXX)
format and has the same lack of error handling as the original.

This also allows ethdev to no longer have a hard dependency
on the cmdline library.

Signed-off-by: Stephen Hemminger <stephen@networkplumber.org>
Reviewed-by: Andrew Rybchenko <arybchenko@solarflare.com>

lib/librte_net/rte_ether.c
@@ -3,6 +3,7 @@
  */
 
 #include <rte_ether.h>
+#include <rte_errno.h>
 
 void
 rte_eth_random_addr(uint8_t *addr)
@@ -26,4 +27,52 @@ rte_ether_format_addr(char *buf, uint16_t size,
 		 eth_addr->addr_bytes[3],
 		 eth_addr->addr_bytes[4],
 		 eth_addr->addr_bytes[5]);
+}
+
+/*
+ * Like ether_aton_r but can handle either
+ * XX:XX:XX:XX:XX:XX or XXXX:XXXX:XXXX
+ */
+int
+rte_ether_unformat_addr(const char *s, struct rte_ether_addr *ea)
+{
+	unsigned int o0, o1, o2, o3, o4, o5;
+	int n;
+
+	n = sscanf(s, "%x:%x:%x:%x:%x:%x",
+		    &o0, &o1, &o2, &o3, &o4, &o5);
+
+	if (n == 6) {
+		/* Standard format XX:XX:XX:XX:XX:XX */
+		if (o0 > UINT8_MAX || o1 > UINT8_MAX || o2 > UINT8_MAX ||
+		    o3 > UINT8_MAX || o4 > UINT8_MAX || o5 > UINT8_MAX) {
+			rte_errno = ERANGE;
+			return -1;
+		}
+
+		ea->addr_bytes[0] = o0;
+		ea->addr_bytes[1] = o1;
+		ea->addr_bytes[2] = o2;
+		ea->addr_bytes[3] = o3;
+		ea->addr_bytes[4] = o4;
+		ea->addr_bytes[5] = o5;
+	} else if (n == 3) {
+		/* Support the format XXXX:XXXX:XXXX */
+		if (o0 > UINT16_MAX || o1 > UINT16_MAX || o2 > UINT16_MAX) {
+			rte_errno = ERANGE;
+			return -1;
+		}
+
+		ea->addr_bytes[0] = o0 >> 8;
+		ea->addr_bytes[1] = o0 & 0xff;
+		ea->addr_bytes[2] = o1 >> 8;
+		ea->addr_bytes[3] = o1 & 0xff;
+		ea->addr_bytes[4] = o2 >> 8;
+		ea->addr_bytes[5] = o2 & 0xff;
+	} else {
+		/* unknown format */
+		rte_errno = EINVAL;
+		return -1;
+	}
+	return 0;
 }

lib/librte_net/rte_ether.h
@@ -251,6 +251,21 @@ void
 rte_ether_format_addr(char *buf, uint16_t size,
 		      const struct rte_ether_addr *eth_addr);
 /**
+ * Convert string with Ethernet address to an ether_addr.
+ *
+ * @param str
+ *   A pointer to buffer contains the formatted MAC address.
+ * @param eth_addr
+ *   A pointer to a ether_addr structure.
+ * @return
+ *   0 if successful
+ *   -1 and sets rte_errno if invalid string
+ */
+__rte_experimental
+int
+rte_ether_unformat_addr(const char *str, struct rte_ether_addr *eth_addr);
+
+/**
  * Ethernet header: Contains the destination address, source address
  * and frame type.
  */

lib/librte_net/rte_net_version.map
@@ -26,4 +26,5 @@ EXPERIMENTAL {
 
 	rte_net_make_rarp_packet;
 	rte_net_skip_ip6_ext;
+	rte_ether_unformat_addr;
 };

commit 17615e939a8ecc1170b9170bfb0a74f276952cf3

rte_ether: force format string for unformat_addr
rte_ether_unformation_addr is very lax in what it accepts now, including
ethernet addresses formatted ambiguously as "x:xx:x:xx:x:xx".  However,
previously this behavior was enforced via the my_ether_aton which would
fail ambiguously formatted values.

Reported-by: Michael Santana <msantana@redhat.com>
Fixes: 596d31092d32 ("net: add function to convert string to ethernet address")
Signed-off-by: Aaron Conole <aconole@redhat.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

lib/librte_net/rte_ether.c
@@ -45,7 +45,8 @@ rte_ether_unformat_addr(const char *s, struct rte_ether_addr *ea)
 	if (n == 6) {
 		/* Standard format XX:XX:XX:XX:XX:XX */
 		if (o0 > UINT8_MAX || o1 > UINT8_MAX || o2 > UINT8_MAX ||
-		    o3 > UINT8_MAX || o4 > UINT8_MAX || o5 > UINT8_MAX) {
+		    o3 > UINT8_MAX || o4 > UINT8_MAX || o5 > UINT8_MAX ||
+		    strlen(s) != RTE_ETHER_ADDR_FMT_SIZE - 1) {
 			rte_errno = ERANGE;
 			return -1;
 		}
@@ -58,7 +59,8 @@ rte_ether_unformat_addr(const char *s, struct rte_ether_addr *ea)
 		ea->addr_bytes[5] = o5;
 	} else if (n == 3) {
 		/* Support the format XXXX:XXXX:XXXX */
-		if (o0 > UINT16_MAX || o1 > UINT16_MAX || o2 > UINT16_MAX) {
+		if (o0 > UINT16_MAX || o1 > UINT16_MAX || o2 > UINT16_MAX ||
+		    strlen(s) != RTE_ETHER_ADDR_FMT_SIZE - 4) {
 			rte_errno = ERANGE;
 			return -1;
 		}