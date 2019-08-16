8060467da94202a67f115c936984db4b63a7c979
Konstantin Ananyev <konstantin.ananyev@intel.com>
2019-08-16 13:53:04 +0100

    examples/bpf: remove duplicate mbuf definition
    Get rid of duplicate definitions for rte_mbuf and related macros.
    Include rte_mbuf_core.h instead.
    
    Signed-off-by: Konstantin Ananyev <konstantin.ananyev@intel.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- examples/bpf/t3.c
+++ examples/bpf/t3.c
@@ -22,7 +22,8 @@
 #include <stdio.h>
 #include <net/ethernet.h>
 #include <rte_config.h>
-#include "mbuf.h"
+#include <generic/rte_atomic.h>
+#include <rte_mbuf_core.h>
 #include <arpa/inet.h>
 
 extern void rte_pktmbuf_dump(FILE *, const struct rte_mbuf *, unsigned int);
