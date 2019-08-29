012c77a9193dd74867296432da99c1b6fab79aca
JinYu <jin.yu@intel.com>
2019-08-29 22:12:19 +0800

    vhost: checkout and cleanup the resubmit inflight information
    This patch shows how to checkout the inflight ring and construct
    the resubmit information also include destroying resubmit info.
    
    Signed-off-by: Lin Li <lilin24@baidu.com>
    Signed-off-by: Xun Ni <nixun@baidu.com>
    Signed-off-by: Yu Zhang <zhangyu31@baidu.com>
    Signed-off-by: Jin Yu <jin.yu@intel.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/librte_vhost/rte_vhost.h
+++ lib/librte_vhost/rte_vhost.h
@@ -146,6 +146,25 @@ struct inflight_info_packed {
 	struct inflight_desc_packed desc[0];
 };
 
+struct rte_vhost_resubmit_desc {
+	uint16_t index;
+	uint64_t counter;
+};
+
+struct rte_vhost_resubmit_info {
+	struct rte_vhost_resubmit_desc *resubmit_list;
+	uint16_t resubmit_num;
+};
+
+struct rte_vhost_ring_inflight {
+	union {
+		struct inflight_info_split *inflight_split;
+		struct inflight_info_packed *inflight_packed;
+	};
+
+	struct rte_vhost_resubmit_info *resubmit_inflight;
+};
+
 struct rte_vhost_vring {
 	union {
 		struct vring_desc *desc;
