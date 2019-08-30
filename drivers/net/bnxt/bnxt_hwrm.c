3159847aafc3206bc749ef39c49c5c24361bc96c
Kalesh AP <kalesh-anakkur.purayil@broadcom.com>
2019-08-30 09:35:32 -0700

    net/bnxt: advertise error recovery capability and handle async event
    1. Advertise HWRM_FUNC_DRV_RGTR_INPUT_FLAGS_ERROR_RECOVERY_SUPPORT flag
       in the FUNC_DRV_RGTR command.
    2. request for the async event ASYNC_EVENT_CMPL_EVENT_ID_ERROR_RECOVERY
       in the FUNC_DRV_RGTR command.
    3. handle the async event EVENT_ID_ERROR_RECOVERY from FW.
    
    Error recovery support will be used by firmware only if all the driver
    instances support error recovery process.
    
    Signed-off-by: Kalesh AP <kalesh-anakkur.purayil@broadcom.com>
    Reviewed-by: Somnath Kotur <somnath.kotur@broadcom.com>
    Signed-off-by: Ajit Khaparde <ajit.khaparde@broadcom.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- drivers/net/bnxt/bnxt_hwrm.c
+++ drivers/net/bnxt/bnxt_hwrm.c
@@ -685,6 +685,8 @@ int bnxt_hwrm_func_driver_register(struct bnxt *bp)
 		return 0;
 
 	flags = HWRM_FUNC_DRV_RGTR_INPUT_FLAGS_HOT_RESET_SUPPORT;
+	if (bp->flags & BNXT_FLAG_FW_CAP_ERROR_RECOVERY)
+		flags |= HWRM_FUNC_DRV_RGTR_INPUT_FLAGS_ERROR_RECOVERY_SUPPORT;
 
 	HWRM_PREP(req, FUNC_DRV_RGTR, BNXT_USE_CHIMP_MB);
 	req.enables = rte_cpu_to_le_32(HWRM_FUNC_DRV_RGTR_INPUT_ENABLES_VER |
@@ -716,6 +718,9 @@ int bnxt_hwrm_func_driver_register(struct bnxt *bp)
 				 ASYNC_CMPL_EVENT_ID_PORT_CONN_NOT_ALLOWED |
 				 ASYNC_CMPL_EVENT_ID_LINK_SPEED_CFG_CHANGE |
 				 ASYNC_CMPL_EVENT_ID_RESET_NOTIFY);
+	if (bp->flags & BNXT_FLAG_FW_CAP_ERROR_RECOVERY)
+		req.async_event_fwd[0] |=
+			rte_cpu_to_le_32(ASYNC_CMPL_EVENT_ID_ERROR_RECOVERY);
 	req.async_event_fwd[1] |=
 		rte_cpu_to_le_32(ASYNC_CMPL_EVENT_ID_PF_DRVR_UNLOAD |
 				 ASYNC_CMPL_EVENT_ID_VF_CFG_CHANGE);
