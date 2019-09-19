9835bd358addcaa4c989fde845f533b763a45240
Sunil Kumar Kori <skori@marvell.com>
2019-09-19 14:55:58 +0530

    examples/l2fwd-event: add eth port setup for eventdev
    Add ethernet port Rx/Tx queue setup for event device which are later
    used for setting up event eth Rx/Tx adapters.
    
    Signed-off-by: Sunil Kumar Kori <skori@marvell.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- examples/l2fwd-event/l2fwd_eventdev.c
+++ examples/l2fwd-event/l2fwd_eventdev.c
@@ -19,6 +19,14 @@
 #include "l2fwd_eventdev.h"
 
 static void
+print_ethaddr(const char *name, const struct rte_ether_addr *eth_addr)
+{
+	char buf[RTE_ETHER_ADDR_FMT_SIZE];
+	rte_ether_format_addr(buf, RTE_ETHER_ADDR_FMT_SIZE, eth_addr);
+	printf("%s%s", name, buf);
+}
+
+static void
 parse_mode(const char *optarg)
 {
 	struct eventdev_resources *eventdev_rsrc = get_eventdev_rsrc();
@@ -77,6 +85,108 @@ parse_eventdev_args(char **argv, int argc)
 }
 
 static void
+eth_dev_port_setup(uint16_t ethdev_count __rte_unused)
+{
+	struct eventdev_resources *eventdev_rsrc = get_eventdev_rsrc();
+	static struct rte_eth_conf port_config = {
+		.rxmode = {
+			.mq_mode = ETH_MQ_RX_RSS,
+			.max_rx_pkt_len = RTE_ETHER_MAX_LEN,
+			.split_hdr_size = 0,
+			.offloads = DEV_RX_OFFLOAD_CHECKSUM
+		},
+		.rx_adv_conf = {
+			.rss_conf = {
+				.rss_key = NULL,
+				.rss_hf = ETH_RSS_IP,
+			}
+		},
+		.txmode = {
+			.mq_mode = ETH_MQ_TX_NONE,
+		}
+	};
+	struct rte_eth_conf local_port_conf;
+	struct rte_eth_dev_info dev_info;
+	struct rte_eth_txconf txconf;
+	struct rte_eth_rxconf rxconf;
+	uint16_t nb_rxd = RTE_TEST_RX_DESC_DEFAULT;
+	uint16_t nb_txd = RTE_TEST_TX_DESC_DEFAULT;
+	uint16_t port_id;
+	int32_t ret;
+
+	/* initialize all ports */
+	RTE_ETH_FOREACH_DEV(port_id) {
+		local_port_conf = port_config;
+		/* skip ports that are not enabled */
+		if ((eventdev_rsrc->port_mask & (1 << port_id)) == 0) {
+			printf("\nSkipping disabled port %d\n", port_id);
+			continue;
+		}
+
+		/* init port */
+		printf("Initializing port %d ... ", port_id);
+		fflush(stdout);
+		rte_eth_dev_info_get(port_id, &dev_info);
+		if (dev_info.tx_offload_capa & DEV_TX_OFFLOAD_MBUF_FAST_FREE)
+			local_port_conf.txmode.offloads |=
+						DEV_TX_OFFLOAD_MBUF_FAST_FREE;
+
+		local_port_conf.rx_adv_conf.rss_conf.rss_hf &=
+						dev_info.flow_type_rss_offloads;
+		if (local_port_conf.rx_adv_conf.rss_conf.rss_hf !=
+				port_config.rx_adv_conf.rss_conf.rss_hf) {
+			printf("Port %u modified RSS hash function "
+			       "based on hardware support,"
+			       "requested:%#"PRIx64" configured:%#"PRIx64"\n",
+			       port_id,
+			       port_config.rx_adv_conf.rss_conf.rss_hf,
+			       local_port_conf.rx_adv_conf.rss_conf.rss_hf);
+		}
+
+		ret = rte_eth_dev_configure(port_id, 1, 1, &local_port_conf);
+		if (ret < 0)
+			rte_exit(EXIT_FAILURE,
+				 "Cannot configure device: err=%d, port=%d\n",
+				 ret, port_id);
+
+		ret = rte_eth_dev_adjust_nb_rx_tx_desc(port_id, &nb_rxd,
+						       &nb_txd);
+		if (ret < 0)
+			rte_exit(EXIT_FAILURE,
+				 "Cannot adjust number of descriptors: err=%d, "
+				 "port=%d\n", ret, port_id);
+
+		rte_eth_macaddr_get(port_id,
+				    &eventdev_rsrc->ports_eth_addr[port_id]);
+		print_ethaddr(" Address:",
+			      &eventdev_rsrc->ports_eth_addr[port_id]);
+		printf("\n");
+
+
+		/* init one Rx queue per port */
+		rxconf = dev_info.default_rxconf;
+		rxconf.offloads = local_port_conf.rxmode.offloads;
+		ret = rte_eth_rx_queue_setup(port_id, 0, nb_rxd, 0, &rxconf,
+					     eventdev_rsrc->pkt_pool);
+		if (ret < 0)
+			rte_exit(EXIT_FAILURE,
+				 "rte_eth_rx_queue_setup: err=%d, "
+				 "port=%d\n", ret, port_id);
+
+		/* init one Tx queue per port */
+		txconf = dev_info.default_txconf;
+		txconf.offloads = local_port_conf.txmode.offloads;
+		ret = rte_eth_tx_queue_setup(port_id, 0, nb_txd, 0, &txconf);
+		if (ret < 0)
+			rte_exit(EXIT_FAILURE,
+				 "rte_eth_tx_queue_setup: err=%d, "
+				 "port=%d\n", ret, port_id);
+
+		rte_eth_promiscuous_enable(port_id);
+	}
+}
+
+static void
 eventdev_capability_setup(void)
 {
 	struct eventdev_resources *eventdev_rsrc = get_eventdev_rsrc();
@@ -105,6 +215,7 @@ void
 eventdev_resource_setup(void)
 {
 	struct eventdev_resources *eventdev_rsrc = get_eventdev_rsrc();
+	uint16_t ethdev_count = rte_eth_dev_count_avail();
 	uint32_t service_id;
 	int32_t ret;
 
@@ -118,6 +229,9 @@ eventdev_resource_setup(void)
 
 	/* Setup eventdev capability callbacks */
 	eventdev_capability_setup();
+
+	/* Ethernet device configuration */
+	eth_dev_port_setup(ethdev_count);
 
 	/* Start event device service */
 	ret = rte_event_dev_service_id_get(eventdev_rsrc->event_d_id,
