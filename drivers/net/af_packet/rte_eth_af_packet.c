Abhisheknishant/dpdk

commit 74b7fc0a0ff151d7160171cfb0f712dcdd704505

net/af_packet: fix packet bytes counting
On error, we also need to zero the bytes transmitted.

Fixes: f4ff17a465d2 ("net/af_packet: guard against buffer overruns in Tx path")
Cc: stable@dpdk.org

Signed-off-by: Chas Williams <ciwillia@brocade.com>

drivers/net/af_packet/rte_eth_af_packet.c
@@ -252,8 +252,11 @@ eth_af_packet_tx(void *queue, struct rte_mbuf **bufs, uint16_t nb_pkts)
 	}
 
 	/* kick-off transmits */
-	if (sendto(pkt_q->sockfd, NULL, 0, MSG_DONTWAIT, NULL, 0) == -1)
-		num_tx = 0; /* error sending -- no packets transmitted */
+	if (sendto(pkt_q->sockfd, NULL, 0, MSG_DONTWAIT, NULL, 0) == -1) {
+		/* error sending -- no packets transmitted */
+		num_tx = 0;
+		num_tx_bytes = 0;
+	}
 
 	pkt_q->framenum = framenum;
 	pkt_q->tx_pkts += num_tx;

commit a15cf880077328241af8869f3b79927af565c1f2

net/af_packet: improve Tx statistics accuracy
When sendto call fails and ENOBUFS error is being set some of the
packets are actually successfully transmitted. There is no available
count of those packets, so in order to make the statistics more
accurate, all the previously enqueued packets will be considered
successful, even though this is not entirely correct.

Before:
   testpmd Tx statistics:
      TX-packets: 7529062   TX-errors: 3702150  TX-bytes:  451743720
   pktgen Rx statistics:
      Total Rx Pkts: 10700700

After:
   testpmd TX statistics:
      TX-packets: 11510625  TX-errors: 0        TX-bytes:  690637500
   pktgen Rx statistics:
      Total Rx Pkts: 10974307

Fixes: 74b7fc0a0ff1 ("net/af_packet: fix packet bytes counting")
Cc: ciwillia@brocade.com
Cc: stable@dpdk.org

Signed-off-by: Flavia Musatescu <flavia.musatescu@intel.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

drivers/net/af_packet/rte_eth_af_packet.c
@@ -244,8 +244,16 @@ eth_af_packet_tx(void *queue, struct rte_mbuf **bufs, uint16_t nb_pkts)
 	}
 
 	/* kick-off transmits */
-	if (sendto(pkt_q->sockfd, NULL, 0, MSG_DONTWAIT, NULL, 0) == -1) {
-		/* error sending -- no packets transmitted */
+	if (sendto(pkt_q->sockfd, NULL, 0, MSG_DONTWAIT, NULL, 0) == -1 &&
+			errno != ENOBUFS) {
+		/* Error sending.
+		 * When sendto call fails and ENOBUFS error is being set
+		 * some of the packets are actually successfully transmitted.
+		 * There is no available count of those packets, so in order
+		 * to make the statistics more accurate, all of the previously
+		 * enqueued packets will be considered successful, even though
+		 * this is not entirely correct.
+		 */
 		num_tx = 0;
 		num_tx_bytes = 0;
 	}