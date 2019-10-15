ce748bd9477a9ed5dc8447092b17e0670945f6a6
Marvin Liu <yong.liu@intel.com>
2019-10-16 00:07:29 +0800

    vhost: try to unroll for each loop
    Create macro for adding unroll pragma before for each loop. Batch
    functions will be contained of several small loops which can be
    optimized by compilers' loop unrolling pragma.
    
    Signed-off-by: Marvin Liu <yong.liu@intel.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/librte_vhost/vhost.h
+++ lib/librte_vhost/vhost.h
@@ -39,6 +39,30 @@
 
 #define VHOST_LOG_CACHE_NR 32
 
+#define PACKED_BATCH_SIZE (RTE_CACHE_LINE_SIZE / \
+			    sizeof(struct vring_packed_desc))
+#define PACKED_BATCH_MASK (PACKED_BATCH_SIZE - 1)
+
+#ifdef SUPPORT_GCC_UNROLL_PRAGMA
+#define for_each_try_unroll(iter, val, size) _Pragma("GCC unroll 4") \
+	for (iter = val; iter < size; iter++)
+#endif
+
+#ifdef SUPPORT_CLANG_UNROLL_PRAGMA
+#define for_each_try_unroll(iter, val, size) _Pragma("unroll 4") \
+	for (iter = val; iter < size; iter++)
+#endif
+
+#ifdef SUPPORT_ICC_UNROLL_PRAGMA
+#define for_each_try_unroll(iter, val, size) _Pragma("unroll (4)") \
+	for (iter = val; iter < size; iter++)
+#endif
+
+#ifndef for_each_try_unroll
+#define for_each_try_unroll(iter, val, num) \
+	for (iter = val; iter < num; iter++)
+#endif
+
 /**
  * Structure contains buffer address, length and descriptor index
  * from vring to do scatter RX.
