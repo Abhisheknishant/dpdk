Abhisheknishant/dpdk

commit f3af3e44a444cdfe3fa7b3e2c042be351401eb23

mk: disable OcteonTx for buggy compilers only on arm64
Disable octeontx for gcc 4.8.5 as the compiler is emitting "internal
compiler error" for aarch64. The GCC "internal compiler error" was
observed only for arm64 architecture so disable the PMD only
for arm64.

Fixes: 4f760550a093 ("mk: disable OcteonTx for buggy compilers")
Cc: stable@dpdk.org

Signed-off-by: Jerin Jacob <jerin.jacob@caviumnetworks.com>
Acked-by: Ferruh Yigit <ferruh.yigit@intel.com>

mk/toolchain/gcc/rte.toolchain-compat.mk
@@ -79,11 +79,13 @@ else
 		CONFIG_RTE_LIBRTE_PMD_OPDL_EVENTDEV=d
 	endif
 
-	# Disable octeontx event PMD for gcc < 4.8.6
+	# Disable octeontx event PMD for gcc < 4.8.6 & ARCH=arm64
+	ifeq ($(CONFIG_RTE_ARCH), arm64)
 	ifeq ($(shell test $(GCC_VERSION)$(GCC_PATCHLEVEL) -lt 486 && echo 1), 1)
 		CONFIG_RTE_LIBRTE_PMD_OCTEONTX_SSOVF=d
 		CONFIG_RTE_LIBRTE_OCTEONTX_MEMPOOL=d
 		CONFIG_RTE_LIBRTE_OCTEONTX_PMD=d
+	endif
 	endif
 
 endif

commit dfefffc7c4cad83ce4821a225ce990570ec3c50b

mk: fix build on arm64
OcteonTx was disabled for causing an internal compiler error on old gcc
versions.
See commit 4f760550a093 ("mk: disable OcteonTx for buggy compilers").

The condition that was added later to apply disabling OcteonTx
only on arm64 caused the condition to never be obeyed because it
compares ["arm64"] to [arm64].

This fixes the condition by using RTE_ARCH instead of CONFIG_RTE_ARCH,
because the former has the quotes removed, while the later doesn't.

Fixes: f3af3e44a444 ("mk: disable OcteonTx for buggy compilers only on arm64")
Cc: stable@dpdk.org

Signed-off-by: Ali Alnubani <alialnu@mellanox.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

mk/toolchain/gcc/rte.toolchain-compat.mk
@@ -99,7 +99,7 @@ else
 	endif
 
 	# Disable octeontx event PMD for gcc < 4.8.6 & ARCH=arm64
-	ifeq ($(CONFIG_RTE_ARCH), arm64)
+	ifeq ($(RTE_ARCH), arm64)
 	ifeq ($(shell test $(GCC_VERSION)$(GCC_PATCHLEVEL) -lt 486 && echo 1), 1)
 		CONFIG_RTE_LIBRTE_PMD_OCTEONTX_SSOVF=d
 		CONFIG_RTE_LIBRTE_OCTEONTX_MEMPOOL=d