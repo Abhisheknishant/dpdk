e2483f65e77b896882acc9d2954b6e12ae39bbb6
Pavan Nikhilesh <pbhagavatula@marvell.com>
2019-10-29 10:33:10 +0530

    examples/l2fwd: disable ptype parsing
    Disable packet type parsing as l2fwd doesn't rely on packet types.
    
    Signed-off-by: Pavan Nikhilesh <pbhagavatula@marvell.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- examples/l2fwd/main.c
+++ examples/l2fwd/main.c
@@ -713,6 +713,8 @@ main(int argc, char **argv)
 			"Cannot set error callback for tx buffer on port %u\n",
 				 portid);
 
+		rte_eth_dev_set_supported_ptypes(portid, RTE_PTYPE_UNKNOWN,
+						 NULL, 0);
 		/* Start device */
 		ret = rte_eth_dev_start(portid);
 		if (ret < 0)
