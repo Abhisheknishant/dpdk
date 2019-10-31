a1bb7aa739ec5278c63b2aeb3b631a00062c36fa
Yahui Cao <yahui.cao@intel.com>
2019-10-31 18:38:17 +0800

    net/ice: remove GTPU tunnel support for inner L4
    Fixes: efc16c621415 ("net/ice: support flow director GTPU tunnel")
    
    Signed-off-by: Yahui Cao <yahui.cao@intel.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- drivers/net/ice/ice_fdir_filter.c
+++ drivers/net/ice/ice_fdir_filter.c
@@ -128,10 +128,6 @@ static struct ice_pattern_match_item ice_fdir_pattern_comms[] = {
 	{pattern_eth_ipv4_udp_vxlan_eth_ipv4_sctp,
 				       ICE_FDIR_INSET_VXLAN_IPV4_SCTP,       ICE_INSET_NONE},
 	{pattern_eth_ipv4_gtpu_ipv4,   ICE_FDIR_INSET_GTPU_IPV4,             ICE_INSET_NONE},
-	{pattern_eth_ipv4_gtpu_ipv4_tcp,
-				       ICE_FDIR_INSET_GTPU_IPV4,             ICE_INSET_NONE},
-	{pattern_eth_ipv4_gtpu_ipv4_udp,
-				       ICE_FDIR_INSET_GTPU_IPV4,             ICE_INSET_NONE},
 };
 
 static struct ice_flow_parser ice_fdir_parser_os;
