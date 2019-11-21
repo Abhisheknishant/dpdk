5d2b19e383f16684a70e03a0e6b050589cb069aa
Dharmik Thakkar <dharmik.thakkar@arm.com>
2019-11-21 12:17:59 -0600

    lib/hash: remove unnecessary locks in lock-free
    Remove __hash_rw_reader_unlock() calls from lock free hash lookup
    
    Signed-off-by: Dharmik Thakkar <dharmik.thakkar@arm.com>
    Reviewed-by: Gavin Hu <gavin.hu@arm.com>
    Reviewed-by: Honnappa Nagarahalli <honnappa.nagarahalli@arm.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/librte_hash/rte_cuckoo_hash.c
+++ lib/librte_hash/rte_cuckoo_hash.c
@@ -1285,10 +1285,8 @@ __rte_hash_lookup_with_hash_lf(const struct rte_hash *h, const void *key,
 		/* Check if key is in primary location */
 		bkt = &h->buckets[prim_bucket_idx];
 		ret = search_one_bucket_lf(h, key, short_sig, data, bkt);
-		if (ret != -1) {
-			__hash_rw_reader_unlock(h);
+		if (ret != -1)
 			return ret;
-		}
 		/* Calculate secondary hash */
 		bkt = &h->buckets[sec_bucket_idx];
 
@@ -1296,10 +1294,8 @@ __rte_hash_lookup_with_hash_lf(const struct rte_hash *h, const void *key,
 		FOR_EACH_BUCKET(cur_bkt, bkt) {
 			ret = search_one_bucket_lf(h, key, short_sig,
 						data, cur_bkt);
-			if (ret != -1) {
-				__hash_rw_reader_unlock(h);
+			if (ret != -1)
 				return ret;
-			}
 		}
 
 		/* The loads of sig_current in search_one_bucket
