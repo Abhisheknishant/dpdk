Abhisheknishant/dpdk

commit c2c0aa75cd0170cc243baf267622f7c3c5309fd5

net/octeontx2: fix loopback config return for VF
loopback mode is not supported for VFs so returning error if
VF is being configured with loopback mode.

Fixes: 920717e4d8ba ("net/octeontx2: add device start operation")
Cc: stable@dpdk.org

Signed-off-by: Sunil Kumar Kori <skori@marvell.com>
Acked-by: Jerin Jacob <jerinj@marvell.com>

drivers/net/octeontx2/otx2_ethdev.c
@@ -205,7 +205,7 @@ cgx_intlbk_enable(struct otx2_eth_dev *dev, bool en)
 	struct otx2_mbox *mbox = dev->mbox;
 
 	if (otx2_dev_is_vf_or_sdp(dev))
-		return 0;
+		return -ENOTSUP;
 
 	if (en)
 		otx2_mbox_alloc_msg_cgx_intlbk_enable(mbox);

commit 977d327cdb4aaa379319e0326f11e2b94a9749a4

net/octeontx2: fix VF configuration fail
Returning -ENOTSUP only in case loopback mode is enabled and
device is VF or SDP.

Fixes: c2c0aa75cd01 ("net/octeontx2: fix loopback config return for VF")
Cc: stable@dpdk.org

Signed-off-by: Harman Kalra <hkalra@marvell.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

drivers/net/octeontx2/otx2_ethdev.c
@@ -204,7 +204,7 @@ cgx_intlbk_enable(struct otx2_eth_dev *dev, bool en)
 {
 	struct otx2_mbox *mbox = dev->mbox;
 
-	if (otx2_dev_is_vf_or_sdp(dev))
+	if (en && otx2_dev_is_vf_or_sdp(dev))
 		return -ENOTSUP;
 
 	if (en)