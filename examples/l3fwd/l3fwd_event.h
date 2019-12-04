12001276f83bd8e575712ce02ddbd7bfd51e47af
Sunil Kumar Kori <skori@marvell.com>
2019-12-04 20:13:35 +0530

    examples/l3fwd: split pipelines based on capability
    Add infra to split eventdev framework based on event Tx adapter
    capability.
    If event Tx adapter has internal port capability then we use
    `rte_event_eth_tx_adapter_enqueue` to transmitting packets else
    we use a SINGLE_LINK event queue to enqueue packets to a service
    core which is responsible for transmitting packets.
    
    Signed-off-by: Sunil Kumar Kori <skori@marvell.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- examples/l3fwd/l3fwd_event.h
+++ examples/l3fwd/l3fwd_event.h
@@ -7,6 +7,7 @@
 
 #include <rte_common.h>
 #include <rte_eventdev.h>
+#include <rte_event_eth_tx_adapter.h>
 #include <rte_spinlock.h>
 
 #include "l3fwd.h"
@@ -19,8 +20,25 @@ enum {
 	CMD_LINE_OPT_EVENTQ_SYNC_NUM,
 };
 
+typedef uint32_t (*event_device_setup_cb)(void);
+typedef void (*event_queue_setup_cb)(uint32_t event_queue_cfg);
+typedef void (*event_port_setup_cb)(void);
+typedef void (*adapter_setup_cb)(void);
+typedef int (*event_loop_cb)(void *);
+
+struct l3fwd_event_setup_ops {
+	event_device_setup_cb event_device_setup;
+	event_queue_setup_cb event_queue_setup;
+	event_port_setup_cb event_port_setup;
+	adapter_setup_cb adapter_setup;
+	event_loop_cb lpm_event_loop;
+	event_loop_cb em_event_loop;
+};
+
 struct l3fwd_event_resources {
+	struct l3fwd_event_setup_ops ops;
 	uint8_t sched_type;
+	uint8_t tx_mode_q;
 	uint8_t enabled;
 	uint8_t nb_args;
 	char **args;
@@ -50,5 +68,7 @@ l3fwd_get_eventdev_rsrc(void)
 }
 
 void l3fwd_event_resource_setup(void);
+void l3fwd_event_set_generic_ops(struct l3fwd_event_setup_ops *ops);
+void l3fwd_event_set_internal_port_ops(struct l3fwd_event_setup_ops *ops);
 
 #endif /* __L3FWD_EVENTDEV_H__ */
