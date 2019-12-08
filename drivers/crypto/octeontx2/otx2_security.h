4d8548d31e0e6225553778e1f461fd10ff4b8377
Ankur Dwivedi <adwivedi@marvell.com>
2019-12-08 17:24:53 +0530

    crypto/octeontx2: add inline tx path changes
    Adding pre-processing required for inline IPsec outbound packets.
    
    Signed-off-by: Ankur Dwivedi <adwivedi@marvell.com>
    Signed-off-by: Anoob Joseph <anoobj@marvell.com>
    Signed-off-by: Archana Muniganti <marchana@marvell.com>
    Signed-off-by: Tejasree Kondoj <ktejasree@marvell.com>
    Signed-off-by: Vamsi Attunuru <vattunuru@marvell.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- drivers/crypto/octeontx2/otx2_security.h
+++ drivers/crypto/octeontx2/otx2_security.h
@@ -14,6 +14,15 @@
 #define OTX2_MAX_CPT_QP_PER_PORT 64
 #define OTX2_MAX_INLINE_PORTS 64
 
+#define OTX2_CPT_RES_ALIGN		16
+#define OTX2_NIX_SEND_DESC_ALIGN	16
+#define OTX2_CPT_INST_SIZE		64
+
+#define OTX2_CPT_EGRP_INLINE_IPSEC	1
+
+#define OTX2_CPT_OP_INLINE_IPSEC_OUTB	(0x40 | 0x25)
+#define OTX2_CPT_OP_INLINE_IPSEC_INB	(0x40 | 0x26)
+
 struct otx2_cpt_qp;
 
 struct otx2_sec_eth_cfg {
@@ -45,6 +54,42 @@ struct otx2_cpt_res {
 	};
 };
 
+struct otx2_cpt_inst_s {
+	union {
+		struct {
+			/* W0 */
+			uint64_t nixtxl : 3;
+			uint64_t doneint : 1;
+			uint64_t nixtx_addr : 60;
+			/* W1 */
+			uint64_t res_addr : 64;
+			/* W2 */
+			uint64_t tag : 32;
+			uint64_t tt : 2;
+			uint64_t grp : 10;
+			uint64_t rsvd_175_172 : 4;
+			uint64_t rvu_pf_func : 16;
+			/* W3 */
+			uint64_t qord : 1;
+			uint64_t rsvd_194_193 : 2;
+			uint64_t wqe_ptr : 61;
+			/* W4 */
+			uint64_t dlen : 16;
+			uint64_t param2 : 16;
+			uint64_t param1 : 16;
+			uint64_t opcode : 16;
+			/* W5 */
+			uint64_t dptr : 64;
+			/* W6 */
+			uint64_t rptr : 64;
+			/* W7 */
+			uint64_t cptr : 61;
+			uint64_t egrp : 3;
+		};
+		uint64_t u64[8];
+	};
+};
+
 /*
  * Security session for inline IPsec protocol offload. This is private data of
  * inline capable PMD.
@@ -67,6 +112,21 @@ struct otx2_sec_session_ipsec_ip {
 	void *cpt_lmtline;
 	/* CPT LF enqueue register address */
 	rte_iova_t cpt_nq_reg;
+
+	/* Pre calculated lengths and data for a session */
+	uint8_t partial_len;
+	uint8_t roundup_len;
+	uint8_t roundup_byte;
+	uint16_t ip_id;
+	union {
+		uint64_t esn;
+		struct {
+			uint32_t seq;
+			uint32_t esn_hi;
+		};
+	};
+
+	uint64_t inst_w7;
 
 	/* CPT QP used by SA */
 	struct otx2_cpt_qp *qp;
