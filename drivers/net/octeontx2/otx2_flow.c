c786e83ee1f842868eb7713c7030a45e99ccf501
Vamsi Attunuru <vattunuru@marvell.com>
2019-12-08 17:24:54 +0530

    crypto/octeontx2: sync inline tag type cfg with Rx adapter configuration
    Tag type configuration for the inline processed packets is set during
    ethdev configuration, it might conflict with tag type configuration
    done during Rx adapter configuration which would be setup later.
    
    This conflict is fixed as part of flow rule creation by updating
    tag type config of inline same as Rx adapter configured tag type.
    
    Signed-off-by: Ankur Dwivedi <adwivedi@marvell.com>
    Signed-off-by: Anoob Joseph <anoobj@marvell.com>
    Signed-off-by: Archana Muniganti <marchana@marvell.com>
    Signed-off-by: Tejasree Kondoj <ktejasree@marvell.com>
    Signed-off-by: Vamsi Attunuru <vattunuru@marvell.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- drivers/net/octeontx2/otx2_flow.c
+++ drivers/net/octeontx2/otx2_flow.c
@@ -299,6 +299,22 @@ flow_free_rss_action(struct rte_eth_dev *eth_dev,
 	return 0;
 }
 
+static int
+flow_update_sec_tt(struct rte_eth_dev *eth_dev,
+		   const struct rte_flow_action actions[])
+{
+	int rc = 0;
+
+	for (; actions->type != RTE_FLOW_ACTION_TYPE_END; actions++) {
+		if (actions->type == RTE_FLOW_ACTION_TYPE_SECURITY) {
+			if (otx2_sec_idev_ops.update_tag_type != NULL)
+				rc = otx2_sec_idev_ops.update_tag_type(eth_dev);
+			break;
+		}
+	}
+
+	return rc;
+}
 
 static int
 flow_parse_meta_items(__rte_unused struct otx2_parse_state *pst)
@@ -491,6 +507,16 @@ otx2_flow_create(struct rte_eth_dev *dev,
 		goto err_exit;
 	}
 
+	if (hw->rx_offloads & DEV_RX_OFFLOAD_SECURITY) {
+		rc = flow_update_sec_tt(dev, actions);
+		if (rc != 0) {
+			rte_flow_error_set(error, EIO,
+					   RTE_FLOW_ERROR_TYPE_UNSPECIFIED,
+					   NULL,
+					   "Failed to update tt with sec act");
+			goto err_exit;
+		}
+	}
 
 	list = &hw->npc_flow.flow_list[flow->priority];
 	/* List in ascending order of mcam entries */
