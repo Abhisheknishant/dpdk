132fc1ed53ffca8da71e18f75ac707bab26f62e8
Ivan Dyukov <i.dyukov@samsung.com>
2019-12-12 11:50:12 +0300

    net/virtio: add link speed tuning
    Some applications like pktgen use link_speed to calculate transmit
    rate. It limits outcome traffic to hardcoded 10G.
    
    This patch makes link_speed configurable at compile time.
    
    Signed-off-by: Ivan Dyukov <i.dyukov@samsung.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- drivers/net/vhost/rte_eth_vhost.c
+++ drivers/net/vhost/rte_eth_vhost.c
@@ -115,7 +115,7 @@ static struct internal_list_head internal_list =
 static pthread_mutex_t internal_list_lock = PTHREAD_MUTEX_INITIALIZER;
 
 static struct rte_eth_link pmd_link = {
-		.link_speed = 10000,
+		.link_speed = RTE_LIBRTE_VIRTIO_LINK_SPEED,
 		.link_duplex = ETH_LINK_FULL_DUPLEX,
 		.link_status = ETH_LINK_DOWN
 };
