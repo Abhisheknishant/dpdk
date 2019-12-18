f3129a640118543a98e9321226c01feae3dcc8ca
Ruifeng Wang <ruifeng.wang@arm.com>
2019-12-18 13:39:01 +0800

    ci: add travis ci support for aarch64
    Add Travis compilation jobs for aarch64. gcc/clang compilations for
    static/shared libraries are added.
    
    Some limitations for current aarch64 Travis support:
    1. Container is used. Huge page is not available due to security reason.
    2. Missing kernel header package in Xenial distribution.
    
    Solutions to address the limitations:
    1. Not to add unit test for now. And run tests with no-huge in future.
    2. Use Bionic distribution for all aarch64 jobs.
    
    Signed-off-by: Ruifeng Wang <ruifeng.wang@arm.com>
    Reviewed-by: Gavin Hu <gavin.hu@arm.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- .ci/linux-setup.sh
+++ .ci/linux-setup.sh
@@ -3,7 +3,10 @@
 # need to install as 'root' since some of the unit tests won't run without it
 sudo python3 -m pip install --upgrade meson
 
-# setup hugepages
-cat /proc/meminfo
-sudo sh -c 'echo 1024 > /proc/sys/vm/nr_hugepages'
-cat /proc/meminfo
+# hugepage settings are skipped on aarch64 due to environment limitation
+if [ "$TRAVIS_ARCH" != "aarch64" ]; then
+    # setup hugepages
+    cat /proc/meminfo
+    sudo sh -c 'echo 1024 > /proc/sys/vm/nr_hugepages'
+    cat /proc/meminfo
+fi
