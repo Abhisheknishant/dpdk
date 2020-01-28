25966406ef1870cfabf63052daeed48cd12f9e66
Marcin Smoczynski <marcinx.smoczynski@intel.com>
2020-01-28 04:16:37 +0100

    test/crypto: add CPU crypto tests
    Add unit and performance tests for CPU crypto mode currently implemented
    by AESNI-GCM cryptodev. Unit tests cover AES-GCM and GMAC test vectors.
    
    Signed-off-by: Marcin Smoczynski <marcinx.smoczynski@intel.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

---
+++ app/test/cpu_crypto_all_gmac_unit_test_cases.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright(c) 2019 Intel Corporation
+ */
+
+TEST_EXPAND(gmac_test_case_1, SGL_ONE_SEG)
+TEST_EXPAND(gmac_test_case_2, SGL_ONE_SEG)
+TEST_EXPAND(gmac_test_case_3, SGL_ONE_SEG)
