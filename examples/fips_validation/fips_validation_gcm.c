Abhisheknishant/dpdk

commit 07f5e4553293cff259b7c44f1aa7bad7734eaca2

examples/fips_validation: fix cipher length for AES-GCM
Cipher length need to be updated in case of AES-GCM decryption.

Fixes: 4aaad2995e13 ("examples/fips_validation: support GCM parsing")
Cc: stable@dpdk.org

Signed-off-by: Archana Muniganti <marchana@marvell.com>
Signed-off-by: Sucharitha Sarananaga <ssarananaga@marvell.com>
Acked-by: Anoob Joseph <anoobj@marvell.com>

examples/fips_validation/fips_validation_gcm.c
@@ -19,6 +19,7 @@
 #define PTLEN_STR	"PTlen = "
 #define AADLEN_STR	"AADlen = "
 #define TAGLEN_STR	"Taglen = "
+#define CTLEN_STR	"PTlen = "
 
 #define COUNT_STR	"Count = "
 #define KEY_STR		"Key = "
@@ -46,6 +47,7 @@ struct fips_test_callback gcm_interim_vectors[] = {
 		{KEYLEN_STR, parser_read_uint32_bit_val, &vec.aead.key},
 		{IVLEN_STR, parser_read_uint32_bit_val, &vec.iv},
 		{PTLEN_STR, parser_read_uint32_bit_val, &vec.pt},
+		{CTLEN_STR, parser_read_uint32_bit_val, &vec.ct},
 		{AADLEN_STR, parser_read_uint32_bit_val, &vec.aead.aad},
 		{TAGLEN_STR, parser_read_uint32_bit_val,
 				&vec.aead.digest},

commit 3cc04b4dd7dc4b489a2fe0f177b59c70e3f2b3ed

examples/fips_validation: fix typo
This patch fixes the cipher len keyword typo.

Fixes: 07f5e4553293 ("examples/fips_validation: fix cipher length for AES-GCM")
Cc: ssarananaga@marvell.com

Signed-off-by: Fan Zhang <roy.fan.zhang@intel.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

examples/fips_validation/fips_validation_gcm.c
@@ -19,7 +19,7 @@
 #define PTLEN_STR	"PTlen = "
 #define AADLEN_STR	"AADlen = "
 #define TAGLEN_STR	"Taglen = "
-#define CTLEN_STR	"PTlen = "
+#define CTLEN_STR	"CTlen = "
 
 #define COUNT_STR	"Count = "
 #define KEY_STR		"Key = "