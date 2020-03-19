a38e71af95bcf50a39b435bb4f983e13e0fa1ec9
Ciara Power <ciara.power@intel.com>
2020-03-19 17:19:05 +0000

    telemetry: remove existing telemetry files
    This patch removes the existing telemetry files, which are now redundant
    as the new version of telemetry has backward compatibility for their
    functionality.
    
    Signed-off-by: Ciara Power <ciara.power@intel.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/librte_telemetry/rte_telemetry.h
+++ lib/librte_telemetry/rte_telemetry.h
@@ -15,8 +15,9 @@
  * RTE Telemetry
  *
  * The telemetry library provides a method to retrieve statistics from
- * DPDK by sending a JSON encoded message over a socket. DPDK will send
+ * DPDK by sending a request message over a socket. DPDK will send
  * a JSON encoded response containing telemetry data.
+ *
  ***/
 
 /**
@@ -50,56 +51,6 @@ typedef void * (*handler)(void *sock_id);
  * @warning
  * @b EXPERIMENTAL: this API may change without prior notice
  *
- * Initialize Telemetry
- *
- * @return
- *  0 on successful initialisation.
- * @return
- *  -ENOMEM on memory allocation error
- * @return
- *  -EPERM on unknown error failure
- * @return
- *  -EALREADY if Telemetry is already initialised.
- */
-__rte_experimental
-int32_t
-rte_telemetry_init(void);
-
-/**
- * @warning
- * @b EXPERIMENTAL: this API may change without prior notice
- *
- * Clean up and free memory.
- *
- * @return
- *  0 on success
- * @return
- *  -EPERM on failure
- */
-__rte_experimental
-int32_t
-rte_telemetry_cleanup(void);
-
-/**
- * @warning
- * @b EXPERIMENTAL: this API may change without prior notice
- *
- * Runs various tests to ensure telemetry initialisation and register/unregister
- * functions are working correctly.
- *
- * @return
- *  0 on success when all tests have passed
- * @return
- *  -1 on failure when the test has failed
- */
-__rte_experimental
-int32_t
-rte_telemetry_selftest(void);
-
-/**
- * @warning
- * @b EXPERIMENTAL: this API may change without prior notice
- *
  * Used when registering a command and callback function with telemetry.
  *
  * @return
@@ -116,7 +67,7 @@ int rte_telemetry_register_cmd(const char *cmd, telemetry_cb fn);
  * @warning
  * @b EXPERIMENTAL: this API may change without prior notice
  *
- * Initialize new version of Telemetry.
+ * Initialize Telemetry.
  *
  * @return
  *  0 on success.
@@ -124,6 +75,6 @@ int rte_telemetry_register_cmd(const char *cmd, telemetry_cb fn);
  *  -1 on failure.
  */
 __rte_experimental
-int rte_telemetry_new_init(void);
+int rte_telemetry_init(void);
 
 #endif
