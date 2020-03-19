098ed5d7ddb2e4aa8e5221894f835efdaa21f076
Bruce Richardson <bruce.richardson@intel.com>
2020-03-19 17:18:59 +0000

    telemetry: introduce new telemetry functionality
    This patch introduces a new telemetry connection socket and handling
    functionality. Like the existing telemetry implementation (which is
    unaffected by this change) it uses a unix socket, but unlike the
    existing one it does not have a fixed list of commands - instead
    libraries or applications can register telemetry commands and callbacks
    to provide a full-extensible solution for all kinds of telemetry across
    DPDK.
    
    Signed-off-by: Bruce Richardson <bruce.richardson@intel.com>
    Signed-off-by: Ciara Power <ciara.power@intel.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/librte_telemetry/rte_telemetry.h
+++ lib/librte_telemetry/rte_telemetry.h
@@ -3,9 +3,12 @@
  */
 
 #include <stdint.h>
+#include <rte_compat.h>
 
 #ifndef _RTE_TELEMETRY_H_
 #define _RTE_TELEMETRY_H_
+
+#define TELEMETRY_MAX_CALLBACKS 64
 
 /**
  * @file
@@ -15,6 +18,33 @@
  * DPDK by sending a JSON encoded message over a socket. DPDK will send
  * a JSON encoded response containing telemetry data.
  ***/
+
+/**
+ * @warning
+ * @b EXPERIMENTAL: this API may change without prior notice
+ *
+ * This telemetry callback is used when registering a command.
+ * It handles getting and formatting stats to be returned to telemetry when
+ * requested. Stats up to buf_len in length are put in the buffer.
+ *
+ * @return
+ * Length of buffer used on success.
+ * @return
+ * Negative integer on error.
+ */
+typedef int (*telemetry_cb)(const char *cmd, const char *params,
+		char *buffer, int buf_len);
+
+/**
+ * @warning
+ * @b EXPERIMENTAL: this API may change without prior notice
+ *
+ * Used for handling data received over a telemetry socket.
+ *
+ * @return
+ * Void.
+ */
+typedef void * (*handler)(void *sock_id);
 
 /**
  * @warning
@@ -66,4 +96,33 @@ __rte_experimental
 int32_t
 rte_telemetry_selftest(void);
 
+/**
+ * @warning
+ * @b EXPERIMENTAL: this API may change without prior notice
+ *
+ * Used when registering a command and callback function with telemetry.
+ *
+ * @return
+ *  0 on success.
+ * @return
+ *  -EINVAL for invalid parameters failure.
+ *  @return
+ *  -ENOENT if max callbacks limit has been reached.
+ */
+__rte_experimental
+int rte_telemetry_register_cmd(const char *cmd, telemetry_cb fn);
+
+/**
+ * @warning
+ * @b EXPERIMENTAL: this API may change without prior notice
+ *
+ * Initialize new version of Telemetry.
+ *
+ * @return
+ *  0 on success.
+ * @return
+ *  -1 on failure.
+ */
+__rte_experimental
+int rte_telemetry_new_init(void);
 #endif
