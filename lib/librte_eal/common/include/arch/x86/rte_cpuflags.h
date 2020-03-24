637e865b2d9eb5ccb24f46d6c0984b9cd984e0de
Kevin Laatz <kevin.laatz@intel.com>
2020-03-24 11:49:17 +0000

    eal/cpuflags: add avx512 vector popcount
    Add the CPU flag for AVX-512 vector popcount instructions.
    
    Signed-off-by: Harry van Haaren <harry.van.haaren@intel.com>
    Signed-off-by: Kevin Laatz <kevin.laatz@intel.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/librte_eal/common/include/arch/x86/rte_cpuflags.h
+++ lib/librte_eal/common/include/arch/x86/rte_cpuflags.h
@@ -125,6 +125,7 @@ enum rte_cpu_flag_t {
 	RTE_CPUFLAG_VPCLMULQDQ,             /**< Vector Carry-less Multiply */
 	RTE_CPUFLAG_AVX512VNNI,             /**< AVX512 Vector Neural Network Instructions */
 	RTE_CPUFLAG_AVX512BITALG,           /**< AVX512 Bit Algorithms */
+	RTE_CPUFLAG_AVX512VPOPCNTDQ,        /**< AVX512 Vector Popcount */
 
 	/* The last item */
 	RTE_CPUFLAG_NUMFLAGS,               /**< This should always be the last! */
