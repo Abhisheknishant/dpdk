ceb3ea35aea2581d9461aa964799055c76051a65
Kevin Laatz <kevin.laatz@intel.com>
2020-03-24 11:49:10 +0000

    eal/cpuflags: add avx512 vector bit manipulation
    Add the CPU flag for AVX-512 vector bit manipulation instructions.
    
    Signed-off-by: Kevin Laatz <kevin.laatz@intel.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/librte_eal/common/include/arch/x86/rte_cpuflags.h
+++ lib/librte_eal/common/include/arch/x86/rte_cpuflags.h
@@ -118,6 +118,7 @@ enum rte_cpu_flag_t {
 	RTE_CPUFLAG_AVX512CD,               /**< AVX512 Conflict Detection*/
 	RTE_CPUFLAG_AVX512BW,               /**< AVX512 Byte and Word */
 	RTE_CPUFLAG_AVX512VL,               /**< AVX512 Vector Length */
+	RTE_CPUFLAG_AVX512VBMI,             /**< AVX512 Vector Bit Manipulation */
 
 	/* The last item */
 	RTE_CPUFLAG_NUMFLAGS,               /**< This should always be the last! */
