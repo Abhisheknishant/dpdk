1c5f6801f47dcb6d7084f9b98eecf13f338f5a3d
Kevin Laatz <kevin.laatz@intel.com>
2020-03-24 11:49:08 +0000

    eal/cpuflags: add avx512 byte and word
    Add the CPU flag for AVX-512 byte and word instructions.
    
    Signed-off-by: Kevin Laatz <kevin.laatz@intel.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/librte_eal/common/include/arch/x86/rte_cpuflags.h
+++ lib/librte_eal/common/include/arch/x86/rte_cpuflags.h
@@ -116,6 +116,7 @@ enum rte_cpu_flag_t {
 	RTE_CPUFLAG_AVX512DQ,               /**< AVX512 Doubleword and Quadword */
 	RTE_CPUFLAG_AVX512IFMA,             /**< AVX512 Integer Fused Multiply-Add */
 	RTE_CPUFLAG_AVX512CD,               /**< AVX512 Conflict Detection*/
+	RTE_CPUFLAG_AVX512BW,               /**< AVX512 Byte and Word */
 
 	/* The last item */
 	RTE_CPUFLAG_NUMFLAGS,               /**< This should always be the last! */
