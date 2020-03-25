9622436704098f143ed1a22c9751f729fba57b7b
Jerin Jacob <jerinj@marvell.com>
2020-03-26 02:45:46 +0530

    eal/trace: add generic tracepoints
    This patch creates the following generic tracepoint for
    generic tracing when there is no dedicated tracepoint is
    available.
    
    rte_trace_lib_eal_generic_void()
    rte_trace_lib_eal_generic_u64()
    rte_trace_lib_eal_generic_u32()
    rte_trace_lib_eal_generic_u16()
    rte_trace_lib_eal_generic_u8()
    rte_trace_lib_eal_generic_i64()
    rte_trace_lib_eal_generic_i32()
    rte_trace_lib_eal_generic_i16()
    rte_trace_lib_eal_generic_i8()
    rte_trace_lib_eal_generic_int()
    rte_trace_lib_eal_generic_long()
    rte_trace_lib_eal_generic_float()
    rte_trace_lib_eal_generic_double()
    rte_trace_lib_eal_generic_ptr()
    rte_trace_lib_eal_generic_str()
    
    For example, if an application wishes to emit an int datatype,
    it can call rte_trace_lib_eal_generic_int(val) to emit the trace.
    
    Signed-off-by: Jerin Jacob <jerinj@marvell.com>
    Signed-off-by: Sunil Kumar Kori <skori@marvell.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

---
+++ lib/librte_eal/common/include/rte_trace_eal.h
@@ -0,0 +1,122 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright(C) 2020 Marvell International Ltd.
+ */
+
+#ifndef _RTE_TRACE_EAL_H_
+#define _RTE_TRACE_EAL_H_
+
+/**
+ * @file
+ *
+ * API for EAL trace support
+ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <rte_trace.h>
+
+/* Generic */
+RTE_TRACE_POINT(
+	rte_trace_lib_eal_generic_void,
+	RTE_TRACE_POINT_ARGS(void),
+)
+
+RTE_TRACE_POINT(
+	rte_trace_lib_eal_generic_u64,
+	RTE_TRACE_POINT_ARGS(uint64_t in),
+	rte_trace_ctf_u64(in);
+)
+
+RTE_TRACE_POINT(
+	rte_trace_lib_eal_generic_u32,
+	RTE_TRACE_POINT_ARGS(uint32_t in),
+	rte_trace_ctf_u32(in);
+)
+
+RTE_TRACE_POINT(
+	rte_trace_lib_eal_generic_u16,
+	RTE_TRACE_POINT_ARGS(uint16_t in),
+	rte_trace_ctf_u16(in);
+)
+
+RTE_TRACE_POINT(
+	rte_trace_lib_eal_generic_u8,
+	RTE_TRACE_POINT_ARGS(uint8_t in),
+	rte_trace_ctf_u8(in);
+)
+
+RTE_TRACE_POINT(
+	rte_trace_lib_eal_generic_i64,
+	RTE_TRACE_POINT_ARGS(int64_t in),
+	rte_trace_ctf_i64(in);
+)
+
+RTE_TRACE_POINT(
+	rte_trace_lib_eal_generic_i32,
+	RTE_TRACE_POINT_ARGS(int32_t in),
+	rte_trace_ctf_i32(in);
+)
+
+RTE_TRACE_POINT(
+	rte_trace_lib_eal_generic_i16,
+	RTE_TRACE_POINT_ARGS(int16_t in),
+	rte_trace_ctf_i16(in);
+)
+
+RTE_TRACE_POINT(
+	rte_trace_lib_eal_generic_i8,
+	RTE_TRACE_POINT_ARGS(int8_t in),
+	rte_trace_ctf_i8(in);
+)
+
+RTE_TRACE_POINT(
+	rte_trace_lib_eal_generic_int,
+	RTE_TRACE_POINT_ARGS(int in),
+	rte_trace_ctf_int(in);
+)
+
+RTE_TRACE_POINT(
+	rte_trace_lib_eal_generic_long,
+	RTE_TRACE_POINT_ARGS(long in),
+	rte_trace_ctf_long(in);
+)
+
+RTE_TRACE_POINT(
+	rte_trace_lib_eal_generic_float,
+	RTE_TRACE_POINT_ARGS(float in),
+	rte_trace_ctf_float(in);
+)
+
+RTE_TRACE_POINT(
+	rte_trace_lib_eal_generic_double,
+	RTE_TRACE_POINT_ARGS(double in),
+	rte_trace_ctf_double(in);
+)
+
+RTE_TRACE_POINT(
+	rte_trace_lib_eal_generic_ptr,
+	RTE_TRACE_POINT_ARGS(const void *ptr),
+	rte_trace_ctf_ptr(ptr);
+)
+
+RTE_TRACE_POINT(
+	rte_trace_lib_eal_generic_str,
+	RTE_TRACE_POINT_ARGS(const char *str),
+	rte_trace_ctf_string(str);
+)
+
+RTE_TRACE_POINT(
+	rte_trace_lib_eal_generic_func,
+	RTE_TRACE_POINT_ARGS(const char *func),
+	rte_trace_ctf_string(func);
+)
+
+#define RTE_TRACE_LIB_EAL_GENERIC_FUNC rte_trace_lib_eal_generic_func(__func__)
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _RTE_TRACE_EAL_H_ */
