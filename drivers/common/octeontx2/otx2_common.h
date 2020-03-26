1c3e05c6bc57ff82784e39acd4fe69cded273307
Pavan Nikhilesh <pbhagavatula@marvell.com>
2020-03-26 12:04:19 +0530

    mempool/octeontx2: add devargs to lock ctx in cache
    Add device arguments to lock NPA aura and pool contexts in NDC cache.
    The device args take hexadecimal bitmask where each bit represent the
    corresponding aura/pool id.
    Example:
    	-w 0002:02:00.0,npa_lock_mask=0xf // Lock first 4 aura/pool ctx
    
    Signed-off-by: Pavan Nikhilesh <pbhagavatula@marvell.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- drivers/common/octeontx2/otx2_common.h
+++ drivers/common/octeontx2/otx2_common.h
@@ -8,6 +8,7 @@
 #include <rte_atomic.h>
 #include <rte_common.h>
 #include <rte_cycles.h>
+#include <rte_kvargs.h>
 #include <rte_memory.h>
 #include <rte_memzone.h>
 #include <rte_io.h>
@@ -49,6 +50,8 @@
 	 (~0ULL >> (BITS_PER_LONG_LONG - 1 - (h))))
 #endif
 
+#define OTX2_NPA_LOCK_MASK "npa_lock_mask"
+
 /* Compiler attributes */
 #ifndef __hot
 #define __hot   __attribute__((hot))
@@ -65,6 +68,7 @@ struct otx2_idev_cfg {
 		rte_atomic16_t npa_refcnt;
 		uint16_t npa_refcnt_u16;
 	};
+	uint64_t npa_lock_mask;
 };
 
 struct otx2_idev_cfg *otx2_intra_dev_get_cfg(void);
@@ -75,6 +79,7 @@ struct otx2_npa_lf *otx2_npa_lf_obj_get(void);
 void otx2_npa_set_defaults(struct otx2_idev_cfg *idev);
 int otx2_npa_lf_active(void *dev);
 int otx2_npa_lf_obj_ref(void);
+void otx2_parse_common_devargs(struct rte_kvargs *kvlist);
 
 /* Log */
 extern int otx2_logtype_base;
