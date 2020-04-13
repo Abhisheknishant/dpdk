de6291209c4cbbd48d9d5c4ee07dde9267a9434d
Jerin Jacob <jerinj@marvell.com>
2020-04-13 20:30:51 +0530

    eal/trace: create CTF TDSL metadata in memory
    Common trace format(CTF) defines the metadata[1][2] for trace events,
    This patch creates the metadata for the DPDK events in memory and
    latter this will be saved to trace directory on rte_trace_save()
    invocation.
    
    [1] https://diamon.org/ctf/#specification
    [2] https://diamon.org/ctf/#examples
    
    Signed-off-by: Jerin Jacob <jerinj@marvell.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/librte_eal/common/eal_common_trace.c
+++ lib/librte_eal/common/eal_common_trace.c
@@ -55,9 +55,13 @@ eal_trace_init(void)
 	/* Generate UUID ver 4 with total size of events and number of events */
 	trace_uuid_generate();
 
+	/* Generate CTF TDSL metadata */
+	if (trace_metadata_create() < 0)
+		goto fail;
+
 	/* Create trace directory */
 	if (trace_mkdir())
-		goto fail;
+		goto free_meta;
 
 	/* Save current epoch timestamp for future use */
 	if (trace_epoch_time_save() < 0)
@@ -67,6 +71,8 @@ eal_trace_init(void)
 
 	return 0;
 
+free_meta:
+	trace_metadata_destroy();
 fail:
 	trace_err("failed to initialize trace [%s]", rte_strerror(rte_errno));
 	return -rte_errno;
@@ -77,6 +83,7 @@ eal_trace_fini(void)
 {
 	if (rte_trace_global_is_disabled())
 		return;
+	trace_metadata_destroy();
 }
 
 bool
