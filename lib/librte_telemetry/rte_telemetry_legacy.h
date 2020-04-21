f6e07fd2e54d6b7b7d1279f2e9509462ada17de5
Ciara Power <ciara.power@intel.com>
2020-04-21 13:39:44 +0100

    telemetry: introduce telemetry backward compatibility
    The new telemetry will now open a socket using the old telemetry path,
    to ensure backward compatibility. This is not yet initialised, as it
    would clash with the existing telemetry, to be removed in a later patch.
    This means that both old and new telemetry socket interfaces are
    handled in a common way.
    
    Signed-off-by: Ciara Power <ciara.power@intel.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

---
+++ lib/librte_telemetry/rte_telemetry_legacy.h
@@ -0,0 +1,65 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright(c) 2020 Intel Corporation
+ */
+
+#ifndef _RTE_TELEMETRY_LEGACY_H_
+#define _RTE_TELEMETRY_LEGACY_H_
+
+#include <rte_compat.h>
+#include "rte_telemetry.h"
+
+/**
+ * @internal
+ * @warning
+ * @b EXPERIMENTAL: this API may change without prior notice
+
+ * @file
+ * RTE Telemetry Legacy
+ *
+ ***/
+
+/**
+ * @internal
+ * Value representing if data is required for the command
+ */
+enum rte_telemetry_legacy_data_req {
+	DATA_NOT_REQ = 0,
+	DATA_REQ
+};
+
+/**
+ * @internal
+ * Counter for the number of registered legacy callbacks
+ */
+extern int num_legacy_callbacks;
+
+/**
+ * @internal
+ * Used for handling data received over the legacy telemetry socket.
+ *
+ * @return
+ * Void.
+ */
+void *
+legacy_client_handler(void *sock_id);
+
+/**
+ * @internal
+ *
+ * Used when registering a command and callback function with
+ * telemetry legacy support.
+ *
+ * @return
+ *  0 on success.
+ * @return
+ *  -EINVAL for invalid parameters failure.
+ *  @return
+ *  -ENOENT if max callbacks limit has been reached.
+ */
+__rte_experimental
+int
+rte_telemetry_legacy_register(const char *cmd,
+		enum rte_telemetry_legacy_data_req data_req,
+		telemetry_cb fn);
+
+#endif
