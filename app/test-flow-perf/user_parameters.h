788ce312d23f441b5060f056a79b4db8bb1055ec
Wisam Jaddo <wisamm@mellanox.com>
2020-04-30 09:32:45 +0000

    app/test-flow-perf: add flow performance skeleton
    Add flow performance application skeleton.
    
    Signed-off-by: Wisam Jaddo <wisamm@mellanox.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

---
+++ app/test-flow-perf/user_parameters.h
@@ -0,0 +1,16 @@
+/* SPDX-License-Identifier: BSD-3-Claus
+ *
+ * This file will hold the user parameters values
+ *
+ * Copyright 2020 Mellanox Technologies, Ltd
+ */
+
+/** Configuration **/
+#define RXQs 4
+#define TXQs 4
+#define HAIRPIN_QUEUES 4
+#define TOTAL_MBUF_NUM 32000
+#define MBUF_SIZE 2048
+#define MBUF_CACHE_SIZE 512
+#define NR_RXD  256
+#define NR_TXD  256
