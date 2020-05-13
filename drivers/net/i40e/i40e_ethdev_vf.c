Abhisheknishant/dpdk

commit 3c8c0b68b590fe8cd7e4a4d18b997eddd5b8b286

net/i40e: set speed to undefined for default case
During PF/VF link update, a default speed value of 100M will be set
if get_link_info has failed or speed is unknown.

Consequently if PF is put in no-carrier state, VFs will switch to
"in carrier" state due to a link up + a link speed set to 100M
(default value if no speed detected).

To be consistent with linux drivers on which PF and VFs are in
same carrier state, sets default speed to undefined (instead of 100M)
and updates a link status of VF only if link is up and speed is
different from undefined.

Fixes: 4861cde46116 ("i40e: new poll mode driver")
Cc: stable@dpdk.org

Signed-off-by: Laurent Hardy <laurent.hardy@6wind.com>
Reviewed-by: Xiaolong Ye <xiaolong.ye@intel.com>
Acked-by: Qi Zhang <qi.z.zhang@intel.com>

drivers/net/i40e/i40e_ethdev_vf.c
@@ -2141,13 +2141,15 @@ i40evf_dev_link_update(struct rte_eth_dev *dev,
 		new_link.link_speed = ETH_SPEED_NUM_40G;
 		break;
 	default:
-		new_link.link_speed = ETH_SPEED_NUM_100M;
+		new_link.link_speed = ETH_SPEED_NUM_NONE;
 		break;
 	}
 	/* full duplex only */
 	new_link.link_duplex = ETH_LINK_FULL_DUPLEX;
-	new_link.link_status = vf->link_up ? ETH_LINK_UP :
-					     ETH_LINK_DOWN;
+	new_link.link_status = vf->link_up &&
+				new_link.link_speed != ETH_SPEED_NUM_NONE
+				? ETH_LINK_UP
+				: ETH_LINK_DOWN;
 	new_link.link_autoneg =
 		!(dev->data->dev_conf.link_speeds & ETH_LINK_SPEED_FIXED);
 

drivers/net/i40e/i40e_ethdev.c
@@ -2832,7 +2832,7 @@ update_link_aq(struct i40e_hw *hw, struct rte_eth_link *link,
 		status = i40e_aq_get_link_info(hw, enable_lse,
 						&link_status, NULL);
 		if (unlikely(status != I40E_SUCCESS)) {
-			link->link_speed = ETH_SPEED_NUM_100M;
+			link->link_speed = ETH_SPEED_NUM_NONE;
 			link->link_duplex = ETH_LINK_FULL_DUPLEX;
 			PMD_DRV_LOG(ERR, "Failed to get link info");
 			return;
@@ -2866,7 +2866,7 @@ update_link_aq(struct i40e_hw *hw, struct rte_eth_link *link,
 		link->link_speed = ETH_SPEED_NUM_40G;
 		break;
 	default:
-		link->link_speed = ETH_SPEED_NUM_100M;
+		link->link_speed = ETH_SPEED_NUM_NONE;
 		break;
 	}
 }

commit b3be41dca05048d2511f64b462751a3359bcf34f

i40e: i40evf link status is always down with the Intel XL710 driver
It is necessary to convert the link_speed, in Mbps, to the I40E_LINK_SPEED enum

Fixes: 3c8c0b68b590 ("net/i40e: set speed to undefined for default case")
Cc: laurent.hardy@6wind.com

Bugzilla ID: 461

Signed-off-by: scott_wasson <scott_wasson@affirmednetworks.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

drivers/net/i40e/i40e_ethdev_vf.c
@@ -1363,7 +1363,30 @@ i40evf_handle_pf_event(struct rte_eth_dev *dev, uint8_t *msg,
 	case VIRTCHNL_EVENT_LINK_CHANGE:
 		PMD_DRV_LOG(DEBUG, "VIRTCHNL_EVENT_LINK_CHANGE event");
 		vf->link_up = pf_msg->event_data.link_event.link_status;
-		vf->link_speed = pf_msg->event_data.link_event.link_speed;
+
+		switch (pf_msg->event_data.link_event.link_speed) {
+		case ETH_SPEED_NUM_100M:
+			vf->link_speed = I40E_LINK_SPEED_100MB;
+			break;
+		case ETH_SPEED_NUM_1G:
+			vf->link_speed = I40E_LINK_SPEED_1GB;
+			break;
+		case ETH_SPEED_NUM_10G:
+			vf->link_speed = I40E_LINK_SPEED_10GB;
+			break;
+		case ETH_SPEED_NUM_20G:
+			vf->link_speed = I40E_LINK_SPEED_20GB;
+			break;
+		case ETH_SPEED_NUM_25G:
+			vf->link_speed = I40E_LINK_SPEED_25GB;
+			break;
+		case ETH_SPEED_NUM_40G:
+			vf->link_speed = I40E_LINK_SPEED_40GB;
+			break;
+		default:
+			vf->link_speed = I40E_LINK_SPEED_UNKNOWN;
+			break;
+		}
 		break;
 	case VIRTCHNL_EVENT_PF_DRIVER_CLOSE:
 		PMD_DRV_LOG(DEBUG, "VIRTCHNL_EVENT_PF_DRIVER_CLOSE event");